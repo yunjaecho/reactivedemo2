ca474ec01cbf222932030f3bbc08d0104e3af8cf
comp1 <chotom73@gmail.com>
2017-12-26 23:43:59 +0900

    2017.10.26

--- src/main/java/live/PubSub.java
+++ src/main/java/live/PubSub.java
@@ -17,11 +17,42 @@ import java.util.stream.Stream;
 public class PubSub {
     public static void main(String[] args) throws InterruptedException {
         Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a + 1).limit(10).collect(Collectors.toList()));
-        Publisher<Integer>  mapPub = mapPub(pub, (Function<Integer, Integer>) s -> s * 10);
-        pub.subscribe(logSub());
+        Publisher<Integer>  mapPub = mapPub(pub, s -> s * 10);
+        mapPub.subscribe(logSub());
     }
 
-    private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> integerIntegerFunction) {
+    private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> f) {
+        return new Publisher<Integer>() {
+            /*@Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(sub);
+            }*/
+
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        sub.onSubscribe(s);
+                    }
+
+                    @Override
+                    public void onNext(Integer i) {
+                        sub.onNext(f.apply(i));
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        sub.onError(t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        sub.onComplete();
+                    }
+                });
+            }
+        };
     }
 
 
